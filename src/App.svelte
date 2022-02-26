717662218c4b14eac4116c8d7caaecbba583fb24
JunSeong <gentlmanjun@naver.com>
2022-02-26 18:58:14 +0900

    단어 입력받고 타일에 출력 기능 구현

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,69 @@
 <script>
-	export let name;
+	let key = "";
+	let largeKey;
+	let keyCode;
+	let keys = ["names", "spill", ""];
+	let cnt = 0;
+
+	function handleKeydown(event) {
+		key = event.key;
+		largeKey = key.toUpperCase();
+		keyCode = event.keyCode;
+
+		if(keyCode == 8 && cnt > 0){//keyCode 8 = 'Backspace'
+			cnt--;
+			keys[keys.length -1] = keys[keys.length -1].slice(0, -1)
+			console.log(keys[keys.length -1])
+		}
+		else if((keyCode < 91 && keyCode > 64) && cnt < 5){
+			cnt++;
+			keys[keys.length -1] = keys[keys.length -1] + largeKey
+			console.log(keys[keys.length -1])
+		}
+	}
+
+	function makeNewline(event) {
+		key = event.key;
+		keyCode = event.keyCode;
+		if(keyCode = 13){
+
+		}
+	}
 </script>
 
+<svelte:window on:keydown={handleKeydown}/>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div >
+		{#each keys as key}
+		<div class = "tiles">
+			<div class="tile">{key.charAt(0)}</div>
+			<div class="tile">{key.charAt(1)}</div>
+			<div class="tile">{key.charAt(2)}</div>
+			<div class="tile">{key.charAt(3)}</div>
+			<div class="tile">{key.charAt(4)}</div>
+		</div>
+		{:else}
+		<div class="tile">Empty arr error</div>
+		{/each}
+		<div>{cnt}</div>
+	</div>
 </main>
 
 <style>
-	main {
+
+	.tiles {
+		display: flex;
+		justify-content: center;
+	}
+
+	.tile {
+		width: 50px;
+		height: 50px;
+		font-size: 40px;
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+		border: solid;
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		margin: 10px;
 	}
 </style>
